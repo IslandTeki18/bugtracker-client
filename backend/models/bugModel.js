28f546f98e6e9dd9096467f9a430fde661adfe70
Landon <landon@gmetrix.com>
2021-02-18 10:33:37 -0700

    error middleware, user and bug model

---
+++ backend/models/bugModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const bugSchema = mongoose.Schema({
+  title: {
+    type: String,
+  },
+  reproSteps: {
+    type: String,
+  },
+  desc: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["Resolved", "Closed", "Active", "New"],
+    default: "New",
+  },
+  assignmentTo: {
+    type: [String],
+  },
+  priority: {
+    type: Number,
+  },
+  severity: {
+    type: String,
+    enum: ["1 - Critical", "2 - High", "3 - Medium", "4 - Low"],
+  },
+  orginalEstimate: {
+    type: Number,
+    required: true,
+  },
+  remaining: {
+    type: Number,
+  },
+  hoursSpent: {
+    type: Number,
+  },
+  levelOfEffort: {
+    type: Number,
+  },
+  comments: {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    body: {
+      type: String,
+    },
+  },
+});
+
+const Bug = mongoose.model("Bug", bugSchema);
+export default Bug;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js"
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware";
 // Imported routes
 
-dotenv.config()
-const app = express()
-app.use(express.json())
-connectDB()
+dotenv.config();
+const app = express();
+app.use(express.json());
+connectDB();
 
 // app.use routes
 
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 app.listen(
-   PORT,
-   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
