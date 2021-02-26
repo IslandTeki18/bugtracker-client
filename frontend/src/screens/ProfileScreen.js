2ed415e9365cf5279b4471b1538ae74a2daca954
Landon McKell <landon.roney7923@gmail.com>
2021-02-25 21:09:47 -0700

    redo bug list screen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,66 +6,74 @@ const ProfileScreen = () => {
   }, []);
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 offset-md-3">
-            <div className="card m-3">
-              <div className="row no-gutters">
-                <div className="col-md-4 d-flex justify-content-center align-self-center">
-                  <div
-                    className="icon-wrapper d-flex justify-content-center"
-                    style={{ backgroundColor: "green" }}
-                  >
-                    <i className="fas fa-bug icon-font-styles"></i>
-                  </div>
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Bug Title {` `}
-                      <span className="badge badge-success">Solved</span>
-                    </h5>
-                    <p className="card-text">
-                      This is a short description of the bug that the user will
-                      title in this section. This is inform the user what the
-                      but is.
-                    </p>
-                  </div>
-                </div>
+      {/* Profile Controls */}
+      <section className="pt-3" id="profile-controls">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 d-flex">
+              <h3 className="text-white pr-4">User Dashboard</h3>
+              <button className="btn btn-light btn-sm">Settings</button>
+            </div>
+            
+            <div className="col-md-6 text-right">
+              <button className="btn btn-primary btn-sm">Add Bug</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Bug List Table */}
+      <section className="p-3" id="bug-list-table">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="table-responsive">
+                <table className="table table-hover table-borderless table-dark table-sm">
+                  <thead className="thead-light">
+                    <tr>
+                      <th scope="col" style={{width: "80px"}}>ID #</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Project</th>
+                      <th scope="col">Type</th>
+                      <th scope="col">Assigned</th>
+                      <th scope="col">Created</th>
+                      <th scope="col">Finished</th>
+                      <th scope="col"></th>
+                      <th scope="col"></th>
+                      <th scope="col"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row" className="text-truncate">
+                        1
+                      </th>
+                      <td className="text-truncate">Login Function</td>
+                      <td className="text-truncate">Active</td>
+                      <td className="text-truncate">StackTrace</td>
+                      <td className="text-truncate">
+                        <span className="badge badge-success">BUG</span>
+                      </td>
+                      <td className="text-truncate">Landon</td>
+                      <td className="text-truncate">02/24/2021</td>
+                      <td className="text-truncate"></td>
+                      <td className="text-truncate">
+                        <button className="btn btn-info btn-sm">Edit</button>
+                      </td>
+                      <td className="text-truncate">
+                        <button className="btn btn-light btn-light btn-sm">View</button>
+                      </td>
+                      <td className="text-truncate">
+                        <button className="btn btn-danger btn-sm">Delete</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-6 offset-md-3">
-            <div className="card m-3">
-              <div className="row no-gutters">
-                <div className="col-md-4 d-flex justify-content-center align-self-center">
-                  <div
-                    className="icon-wrapper d-flex justify-content-center"
-                    style={{ backgroundColor: "orange" }}
-                  >
-                    <i className="fas fa-exclamation-triangle icon-font-styles"></i>
-                  </div>
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Bug Title {` `}
-                      <span className="badge badge-warning">New Issue</span>
-                    </h5>
-                    <p className="card-text">
-                      This is a short description of the bug that the user will
-                      title in this section. This is inform the user what the
-                      but is.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </section>
     </>
   );
 };
